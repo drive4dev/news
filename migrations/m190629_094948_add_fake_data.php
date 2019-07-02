0aa27638c88ede450fdf36852d85ce8df4989621
m.dubinin <m.dubinin@sibset-team.ru>
2019-07-02 18:57:31 +0700

    last fake data

--- migrations/m190629_094948_add_fake_data.php
+++ migrations/m190629_094948_add_fake_data.php
@@ -14,6 +14,7 @@ class m190629_094948_add_fake_data extends Migration
     {
         $this->insertFakeUsers();
         $this->insertFakecategory();
+        $this->insertFakeNews();
     }
 
     /**
@@ -39,7 +40,7 @@ class m190629_094948_add_fake_data extends Migration
         ]);
     }
 
-    public function insertFakecategory()
+    public function insertFakeCategory()
     {
         $this->insert('category', [
             'title' => 'Root',
@@ -90,5 +91,93 @@ class m190629_094948_add_fake_data extends Migration
             'url' => '/category/6'
         ]);
 
+    }
+
+    public function insertFakeNews()
+    {
+        $this->insert('news', [
+            'created_at' => '2019-07-02 11:30:52',
+            'active' => true,
+            'title' => 'Первая новость',
+            'category_id' => 4,
+            'preview' => 'НовостьНовостьНовостьНовостьНовостьНовостьНовость 1',
+            'content' => 'КонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтент',
+            'slug' => 'pervaa-novost'
+        ]);
+        $this->insert('news', [
+            'created_at' => '2019-06-02 11:30:52',
+            'active' => true,
+            'title' => 'новость2',
+            'category_id' => 2,
+            'preview' => 'новость2новость2новость2новость2',
+            'content' => 'КонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтент 2',
+            'slug' => 'novost2'
+        ]);
+
+        $this->insert('news', [
+            'created_at' => '2019-05-02 11:30:52',
+            'active' => true,
+            'title' => 'Первая новость3',
+            'category_id' => 3,
+            'preview' => 'НовостьНовостьНовостьНовостьНовостьНовостьНовость 3',
+            'content' => 'КонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтент',
+            'slug' => 'pervaa-novost3'
+        ]);
+        $this->insert('news', [
+            'created_at' => '2019-05-03 11:30:52',
+            'active' => true,
+            'title' => 'новость4',
+            'category_id' => 2,
+            'preview' => 'новость2новость2новость2новость4',
+            'content' => 'КонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтент 4',
+            'slug' => 'novost4'
+        ]);
+
+        $this->insert('news', [
+            'created_at' => '2019-05-04 11:30:52',
+            'active' => true,
+            'title' => 'Первая новость5',
+            'category_id' => 5,
+            'preview' => 'НовостьНовостьНовостьНовостьНовостьНовостьНовость 5',
+            'content' => 'КонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтент5',
+            'slug' => 'pervaa-novost5'
+        ]);
+        $this->insert('news', [
+            'created_at' => '2019-05-08 11:30:52',
+            'active' => true,
+            'title' => 'новость6',
+            'category_id' => 4,
+            'preview' => 'новость2новость2новость2новость6',
+            'content' => 'КонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтент 6',
+            'slug' => 'novost2'
+        ]);
+
+        $this->insert('news', [
+            'created_at' => '2019-05-09 11:30:52',
+            'active' => true,
+            'title' => 'Первая новость7',
+            'category_id' => 4,
+            'preview' => 'НовостьНовостьНовостьНовостьНовостьНовостьНовость 7',
+            'content' => 'КонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтент7',
+            'slug' => 'pervaa-novost7'
+        ]);
+        $this->insert('news', [
+            'created_at' => '2019-05-19 11:30:52',
+            'active' => true,
+            'title' => 'Первая новость8',
+            'category_id' => 6,
+            'preview' => 'НовостьНовостьНовостьНовостьНовостьНовостьНовость 8',
+            'content' => 'КонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтент8',
+            'slug' => 'pervaa-novost7'
+        ]);
+        $this->insert('news', [
+            'created_at' => '2019-05-10 11:30:52',
+            'active' => true,
+            'title' => 'Первая новость7',
+            'category_id' => 6,
+            'preview' => 'НовостьНовостьНовостьНовостьНовостьНовостьНовость 9',
+            'content' => 'КонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтентКонтент9',
+            'slug' => 'pervaa-novost9'
+        ]);
     }
 }
